8d17c008508307deab0a6f7e0bb6ac80dd5cb843
dmdmiie <98773911+NapatsornL@users.noreply.github.com>
2024-01-31 11:18:10 +0700

    Update main.dart to be 50/50 local asset image/network images.

--- week4/lib/main.dart
+++ week4/lib/main.dart
@@ -33,37 +33,43 @@ class MyHomePage extends StatelessWidget {
               name: "iPhone",
               description: "iPhone is the stylist phone ever",
               price: 1000,
-              image: "iphone.png"),
+              image: "assets/appimages/iphone.png",
+              isNetworkImage: false),
           ProductBox(
               key: ValueKey(2),
               name: "Pixel",
               description: "Pixel is the most featureful phone ever",
               price: 800,
-              image: "pixel.png"),
+              image: "https://techstory.in/wp-content/uploads/2021/05/E1w_CWtVIAAldKc-1024x1024.jpg",
+              isNetworkImage: true),
           ProductBox(
               key: ValueKey(3),
               name: "Laptop",
               description: "Laptop is most productive development tool",
               price: 2000,
-              image: "laptop.png"),
+              image: "assets/appimages/laptop.png",
+              isNetworkImage: false),
           ProductBox(
               key: ValueKey(4),
               name: "Tablet",
               description: "Tablet is the most useful device ever for meeting",
               price: 1500,
-              image: "tablet.png"),
+              image: "https://th.bing.com/th/id/OIP.c6GXoFBw5pVyK01hsGjkpAHaFN?rs=1&pid=ImgDetMain",
+              isNetworkImage: true),
           ProductBox(
               key: ValueKey(5),
               name: "Pendrive",
               description: "Pendrive is a useful storage medium",
               price: 100,
-              image: "pendrive.png"),
+              image: "assets/appimages/pendrive.png",
+              isNetworkImage: false),
           ProductBox(
               key: ValueKey(6),
               name: "Floppy Drive",
               description: "Floppy drive is a useful rescue storage medium",
               price: 20,
-              image: "floppy.png"),
+              image: "https://th.bing.com/th/id/R.52f7219984fdf56dc288fbab81840a7f?rik=vgi8IFRNzdayiQ&pid=ImgRaw&r=0",
+              isNetworkImage: true),
         ],
       ),
     );
@@ -71,18 +77,20 @@ class MyHomePage extends StatelessWidget {
 }
 
 class ProductBox extends StatelessWidget {
-  ProductBox(
-      {Key? key,
-      required this.name,
-      required this.description,
-      required this.price,
-      required this.image})
-      : super(key: key);
+  ProductBox({
+    Key? key,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.isNetworkImage,
+  }) : super(key: key);
 
   final String name;
   final String description;
   final int price;
   final String image;
+  final bool isNetworkImage;
 
   @override
   Widget build(BuildContext context) {
@@ -93,20 +101,25 @@ class ProductBox extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Image.asset("assets/appimages/" + image),
+            isNetworkImage
+                ? Image.network(image, width: 80, height: 80)
+                : Image.asset(image, width: 80, height: 80),
             Expanded(
               child: Container(
-                  padding: EdgeInsets.all(5),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text(this.name,
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      Text(this.description),
-                      Text("Price: " + this.price.toString()),
-                    ],
-                  )),
-            )
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      this.name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(this.description),
+                    Text("Price: " + this.price.toString()),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
